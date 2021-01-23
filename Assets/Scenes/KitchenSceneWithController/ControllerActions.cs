c02050b7360370d11520d0d2d4dc3eb6a726c3d8
Vishvak Seenichamy <vishvak365@gmail.com>
2021-01-23 12:13:36 -0500

    Init

---
+++ Assets/Scenes/KitchenSceneWithController/ControllerActions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerActions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Text: ");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(OVRInput.Get(OVRInput.Button.One));
+    }
+}
---
+++ Assets/Scenes/StartScreen/StartScreenScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartScreenScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject player;
+    public GameObject block;
+    private Vector3 pos = new Vector3(0, 0, 0);
+    void Start()
+    {
+        // Destroy(block);
+        Physics.gravity = new Vector3(0, 0, 0);
+        player.transform.transform.position = pos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("asdf");
+        pos += new Vector3(.01f, 0, 0);
+        player.transform.transform.position = pos;
+        // if (OVRInput.Get(OVRInput.Button.PrimaryTouchpad))
+        // {
+        //     Destory(block);
+        // }
+        // Debug.Log();
+    }
+}
